da625d4e75d6d36c35cd4f4868d694f7be3daabb
“Michael <mpaterni2@cuc.edu.co>
2017-02-21 21:19:50 -0500

    Inicio Proyecto1

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -2,12 +2,33 @@ package com.example.android.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+    private EditText cajaNombre;
+    private TextView cajaMensaje;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+        cajaNombre = (EditText)findViewById(R.id.TxtNombre);
+        cajaMensaje = (TextView) findViewById(R.id.lblView);
+
+        }
+
+    public void Saludar (View v){
+        String aux;
+        aux = cajaNombre.getText().toString();
+
+        cajaMensaje.setText("Hola "+aux);
     }
+
 }
